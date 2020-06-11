bcc82d1c77986609cb1fcb703c0c55b1dd00f7ac
Blake Mauri <blake_mauri@byui.edu>
2020-06-11 02:58:44 -0700

    Week 05.2

--- src/app/documents/document-detail/document-detail.component.ts
+++ src/app/documents/document-detail/document-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Document } from '../document.model';
+
 @Component({
   selector: 'cms-document-detail',
   templateUrl: './document-detail.component.html',
--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Document } from '../document.model';
 
 @Component({
@@ -10,7 +10,19 @@ export class DocumentListComponent implements OnInit {
 
   constructor() { }
 
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  
+  documents: Document[] = [
+    new Document('1', 'Document 1', 'Description 1', 'https://www.document.com', null),
+    new Document('2', 'Document 2', 'Description 2', 'https://www.document.com', null),
+    new Document('3', 'Document 3', 'Description 3', 'https://www.document.com', null),
+    new Document('4', 'Document 4', 'Description 4', 'https://www.document.com', null)
+
+  ];
+
   ngOnInit(): void {
   }
-
+  onSelected(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
